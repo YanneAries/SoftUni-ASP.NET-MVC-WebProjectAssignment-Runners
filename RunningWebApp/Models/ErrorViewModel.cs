ab671a669958a5c7166a3bad41335ed0e367dee9
Yan40 <Yan40@DESKTOP-H1VP7Q6>
2023-06-18 18:50:49 +0300

    Add project files.

---
+++ RunningWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RunningWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
